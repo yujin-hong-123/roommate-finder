9083280185f5a5124ddc70178b2a17cfd8740614
ShadowCat567 <lv2067@nyu.edu>
2024-03-05 13:34:38 -0500

    set up for profile screen

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Profile from './Profile'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          
+          <Route path="/" element={<Profile />}> </Route>
         </Routes>
       </Router>
     </div>
--- front-end/src/Profile.js
+++ front-end/src/Profile.js
@@ -1,7 +1,11 @@
+import "./Profile.css"
+
 function Profile() {
     return (
       <div className="Profile">
-        
+        <p>"Hello world"</p>
       </div>
     );
   }
+
+export default Profile
