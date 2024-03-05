646418f1f7912c34bfd82d3f3a1fb4b1da4826df
ShadowCat567 <lv2067@nyu.edu>
2024-03-05 14:12:48 -0500

    main structure of my profile page added

--- front-end/src/Profile.js
+++ front-end/src/Profile.js
@@ -3,10 +3,29 @@ import Button from "./Button"
 
 function Profile() {
     return (
-      <div className="Profile">
-        <Button text="Edit Profile"></Button>
-        <Button text="Retake Survey"></Button>
-      </div>
+        <>
+        <div className="Heading">
+            <Button text="Edit Profile"></Button>
+            <Button text="Retake Survey"></Button>
+        </div>
+        <div className="Profile">
+            
+        </div>
+        <div className="About">
+            <p className="AboutText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Etiam consequat ante vel vehicula vulputate. Aliquam maximus turpis ut 
+                porttitor imperdiet. Cras elementum orci id neque tincidunt finibus. 
+                Proin accumsan suscipit elit, at varius eros scelerisque id. 
+                Suspendisse maximus ultricies orci, ac convallis mauris accumsan non. 
+                Nullam vel lectus eget urna pulvinar sodales vitae nec neque. 
+                Maecenas ut erat eget turpis ultricies suscipit. 
+                </p>
+        </div>
+        <div className="Footer">
+            <Button text="Preferences"></Button>
+            <Button text="Logout"></Button>
+        </div>
+        </>
     );
   }
 
