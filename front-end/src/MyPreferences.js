b1945d8c9cba047631871aa6211dd02c9483d23b
Robert (Bobby) Impastato <rdi4217@nyu.edu>
2024-03-06 18:52:11 -0500

    updated mypreferences page

--- front-end/src/MyPreferences.js
+++ front-end/src/MyPreferences.js
@@ -6,6 +6,16 @@ import Header from "./Header";
 function MyPreferences() {
     const navigate = useNavigate();
 
+    // IMPORTANT: THIS IS JUST A PLACEHOLDER FOR STUFF WE WILL HANDLE IN BACKEND
+    const [preferences] = useState({
+        petsPreference: 'No',
+        guestPreference: 'Yes',
+        minRent: 500,
+        maxRent: 1500,
+        desiredRoommates: 2,
+        bedtime: '3am',
+    });
+
 
     const handleSubmit = () => {
         // Handle submission logic here later...
@@ -14,11 +24,20 @@ function MyPreferences() {
 
 
     return (
+        <div>
+            <Header />
+            <h2>My Preferences</h2>
 
+            {/* Displaying preferences */}
+            <p>Are you okay with pets? {preferences.petsPreference}</p>
+            <p>Open to guests? {preferences.guestPreference}</p>
+            <p>Minimum Rent Share: ${preferences.minRent}</p>
+            <p>Maximum Rent Share: ${preferences.maxRent}</p>
+            <p>Desired number of roommates (excluding yourself): {preferences.desiredRoommates}</p>
+            <p>Usual bedtime: {preferences.bedtime}</p>
 
-
-
-        <button onClick={handleSubmit}>Back to my Profile</button>
+            <button onClick={handleSubmit}>Back to My Profile</button>
+        </div>
 
     );
 }
