3591977ba3ae82e21e3b57aee37f7f2389daf1a2
bobbyimpastato <rdi4217@nyu.edu>
2024-03-20 21:55:18 -0400

    hard coded test data to App.Js server backend

--- back-end/App.js
+++ back-end/App.js
@@ -1,4 +1,29 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+console.log("created backend server!!!!!!!!!!!!!!!!")
 
-module.exports = app
+//This is basically just the survey responses, for now we have just 1
+//Once we are able to ge this to send properly
+//we will make this a list of several JSON objects
+//and the JSON Objects will be sorted according to
+//how much they match with the current user
+//Then we just send the whole SORTED list of JSON objects to the frontend 
+const hardcodedJSONData = {
+    "user_id": "rkTV8JXlO1",
+    "pets": "no",
+    "guests": "yes",
+    "rent_max": 10000,
+    "rent_min": 300,
+    "bedtime": "irregular"
+};
+
+app.get("/route1", (req, res) => {
+    const hardcodedString = "This is some hardcoded data from the backend to be displayed in Matches";
+    res.json({ data: hardcodedString });
+});
+
+app.get("/route2", (req, res) => {
+    res.json({ data: hardcodedJSONData });
+});
+
+module.exports = app;
--- front-end/src/Matches.js
+++ front-end/src/Matches.js
@@ -8,60 +8,60 @@ import profilepic from './ProfilePic.png';
 function Matches() {
   const navigate = useNavigate();
 
-    return (
-      <div className="MatchList">
-        <Header />
-        <button onClick={() => navigate('/otheruser')} className="rowbutton">
-          <img src={profilepic} className="profilepic_match" alt="profilepic" />
-          <ul className="matchentry">
-            <li className="username_match">Name1</li>
-            <li className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit..</li>
-          </ul>
-        </button>
-        <button onClick={() => navigate('/otheruser')} className="rowbutton">
-          <img src={profilepic} className="profilepic_match" alt="profilepic" />
-          <ul className="matchentry">
-            <li className="username_match">Name2</li>
-            <li className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit..</li>
-          </ul>
-        </button>
-        <button onClick={() => navigate('/otheruser')} className="rowbutton">
-          <img src={profilepic} className="profilepic_match" alt="profilepic" />
-          <ul className="matchentry">
-            <li className="username_match">Name3</li>
-            <li className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit..</li>
-          </ul>
-        </button>
-        <button onClick={() => navigate('/otheruser')} className="rowbutton">
-          <img src={profilepic} className="profilepic_match" alt="profilepic" />
-          <ul className="matchentry">
-            <li className="username_match">Name4</li>
-            <li className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit..</li>
-          </ul>
-        </button>
-        <button onClick={() => navigate('/otheruser')} className="rowbutton">
-          <img src={profilepic} className="profilepic_match" alt="profilepic" />
-          <ul className="matchentry">
-            <li className="username_match">Name5</li>
-            <li className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit..</li>
-          </ul>
-        </button>
-        <button onClick={() => navigate('/otheruser')} className="rowbutton">
-          <img src={profilepic} className="profilepic_match" alt="profilepic" />
-          <ul className="matchentry">
-            <li className="username_match">Name6</li>
-            <li className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit..</li>
-          </ul>
-        </button>
-        <button onClick={() => navigate('/otheruser')} className="rowbutton">
-          <img src={profilepic} className="profilepic_match" alt="profilepic" />
-          <ul className="matchentry">
-            <li className="username_match">Name7</li>
-            <li className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit..</li>
-          </ul>
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="MatchList">
+      <Header />
+      <button onClick={() => navigate('/otheruser')} className="rowbutton">
+        <img src={profilepic} className="profilepic_match" alt="profilepic" />
+        <ul className="matchentry">
+          <li className="username_match">Name1</li>
+          <li className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit..</li>
+        </ul>
+      </button>
+      <button onClick={() => navigate('/otheruser')} className="rowbutton">
+        <img src={profilepic} className="profilepic_match" alt="profilepic" />
+        <ul className="matchentry">
+          <li className="username_match">Name2</li>
+          <li className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit..</li>
+        </ul>
+      </button>
+      <button onClick={() => navigate('/otheruser')} className="rowbutton">
+        <img src={profilepic} className="profilepic_match" alt="profilepic" />
+        <ul className="matchentry">
+          <li className="username_match">Name3</li>
+          <li className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit..</li>
+        </ul>
+      </button>
+      <button onClick={() => navigate('/otheruser')} className="rowbutton">
+        <img src={profilepic} className="profilepic_match" alt="profilepic" />
+        <ul className="matchentry">
+          <li className="username_match">Name4</li>
+          <li className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit..</li>
+        </ul>
+      </button>
+      <button onClick={() => navigate('/otheruser')} className="rowbutton">
+        <img src={profilepic} className="profilepic_match" alt="profilepic" />
+        <ul className="matchentry">
+          <li className="username_match">Name5</li>
+          <li className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit..</li>
+        </ul>
+      </button>
+      <button onClick={() => navigate('/otheruser')} className="rowbutton">
+        <img src={profilepic} className="profilepic_match" alt="profilepic" />
+        <ul className="matchentry">
+          <li className="username_match">Name6</li>
+          <li className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit..</li>
+        </ul>
+      </button>
+      <button onClick={() => navigate('/otheruser')} className="rowbutton">
+        <img src={profilepic} className="profilepic_match" alt="profilepic" />
+        <ul className="matchentry">
+          <li className="username_match">Name7</li>
+          <li className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit..</li>
+        </ul>
+      </button>
+    </div>
+  );
+}
 
 export default Matches
