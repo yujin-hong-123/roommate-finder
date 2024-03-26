f80f6f078f0829d2e4f227fc9d95e003c6390770
Yujin Hong <eh2843@nyu.edu>
2024-03-26 00:12:13 -0400

    working on compat algo

---
+++ back-end/Compatibility.js
@@ -0,0 +1,40 @@
+const prefSarah = {
+    "user_id": "rkTV8JXlO1",
+    //must match
+    "gender": "okay", //same, okay
+    "pets": "okay", //no, okay
+    //rent price, but not always split equally
+    "rent_max": 10000,
+    "rent_min": 100,
+    //rules
+    "guests": "okay", //no, okay
+    "smoke": "no",
+    "drink": "okay",
+    //living patterns
+    "bedtime": "anytime", //early(before 12), late(after 12), anytime(depends)
+    "quiet" : "moderate", //quiet, moderate, loud
+    "cleanliness": "clean", //clean, moderate, messy
+}
+
+const ansJames = {
+    "user_id": "0DkLGCLFym",
+    //must match
+    "gender": "male", //same, okay
+    "pets": "yes", //no, okay
+    //rent price, but not always split equally
+    "rent_max": 5000,
+    "rent_min": 300,
+    //rules
+    "guests": "yes", //yes, no, okay(doesn't matter)
+    "smoke": "no",
+    "drink": "okay",
+    //living patterns
+    "bedtime": "anytime", //early(before 12), late(after 12), anytime(depends)
+    "quiet" : "moderate", //quiet, moderate, loud
+    "cleanliness": "clean", //clean, moderate, messy
+}
+
+function must(){
+}
+
+console.log()
