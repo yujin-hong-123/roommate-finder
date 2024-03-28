14b89d417dc211b339cb6538bbe551b00db4150c
ShadowCat567 <lv2067@nyu.edu>
2024-03-28 00:35:38 -0400

    unit test for survey data post request

--- back-end/test.js
+++ back-end/test.js
@@ -7,7 +7,7 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe("/GET profile info", () => {
-    it("it should GET profile information", (done) => {
+    it("should GET profile information", (done) => {
       chai.request(app)
       .get('/profile')
       .end((err, res) => {
@@ -17,3 +17,31 @@ describe("/GET profile info", () => {
       });
     })
 });
+
+describe("/POST Survey data", () => {
+    it("should acknowledge successful POSt requests to the survey", (done) => {
+       const testSurvey = {
+            petsPreference: 'yes',
+            guestPreference: 'no',
+            minRent: 900,
+            maxRent: 1500,
+            desiredRoommates: 1,
+            bedtime: '3am'
+       }
+       chai.request(app)
+       .post('/survey')
+       .send(testSurvey)
+       .end((err, res) => {
+         res.should.have.status(200);
+         res.body.should.be.a('object');
+         //this will need to be updated if the survey route returns anything
+         done();
+       });
+    });
+})
+
+describe("/GET Chat list info", () => {
+    it("should GET an array of all currently active chats", (done) => {
+        
+    });
+})
