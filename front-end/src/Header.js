566fc0743695aa9e337e39493bf3e7ecbc5498df
Yujin Hong <eh2843@nyu.edu>
2024-04-28 22:04:54 -0400

    icons implemented

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -1,5 +1,9 @@
 import './Header.css'
 import { Link } from 'react-router-dom'
+import logo from './Logo.png'
+import matchesicon from './MatchesIcon.png'
+import chatsicon from './ChatsIcon.png'
+import profileicon from './ProfileIcon.png'
 
 /**
  * A React component that is used for the header displayed at the top of every page of the site.
@@ -11,14 +15,17 @@ const Header = props => {
     <header className="Header-header">
       <nav className="Header-navbar">
         <ul className="nav-links">
-          <li className="nav-item">
-            <Link to="/matches">Matches</Link>
+          <li className ='nav-item'>
+            <img className='icons_logo'src={logo}/>
           </li>
           <li className="nav-item">
-            <Link to="/chatlist">Chats</Link>
+            <Link to="/matches"><img className='icons'src={matchesicon}/></Link>
           </li>
           <li className="nav-item">
-            <Link to="/profile">Profile</Link>
+            <Link to="/chatlist"><img className='icons'src={chatsicon}/></Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/profile"><img className='icons'src={profileicon}/></Link>
           </li>
         </ul>
       </nav>
